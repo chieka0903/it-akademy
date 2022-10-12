596cd85035696993eeeb78efc0ccc6e95f7a3940
Yekaterina Chistova <chieka0903@gmail.com>
2022-10-13 00:10:49 +0200

    add N13

---
+++ N05_TREESUM/N13_DRINKS_HASH_FUNC/test.js
@@ -0,0 +1,64 @@
+function HashStorageFunc() {
+
+    let storage={};
+
+    this.addValue = function(key,value) {
+        storage[key]=value;
+    }
+
+    this.getValue = function(key) {
+        return storage[key];
+    }
+
+    this.deleteValue = function(key) {
+        if (key in storage) {
+            delete storage[key];
+            return true;
+        }
+        else
+            return false;
+       
+    }
+
+    this.getKeys = function() {
+        return Object.keys(storage);
+    }
+}
+
+let drinkStorage = new HashStorageFunc;
+
+function addDrink( ) {
+    let drinkName = prompt ('Введите название напитка');
+    let drinkRecept = prompt ('Ведите рецепт напитка');
+    let alco = confirm ('напиток алкогольный?');
+    let info = {
+        r: drinkRecept,
+        a: alco
+    }
+    drinkStorage.addValue(drinkName,info);
+   
+}
+
+function getDrink() {
+    let drinkName = prompt ('Введите название напитка');
+    if ( drinkName in drinkStorage ){
+        return true
+    }
+    console.log(drinkStorage.getValue);
+       
+    }
+
+function deleteDrink() {
+    let drinkName = prompt ('Введите название напитка');
+    drinkStorage.deleteValue();
+}
+
+function allDrinks() {
+   drinkStorage.getKeys();
+    alert('Название напитка: ' + drinkName + `\n` +
+                'Рецепт напитка: ' + drinkRecept + `\n` +
+                'Алкогольный: ' + alco + `\n`);
+}
+
+
+
